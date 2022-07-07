82e57fa413916a04b01da95f63f619bb83c6bfe4
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-07-07 14:41:32 +0000

    chore: setup eslint with airbnb for typescript

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/no-unresolved': 'off',
+    'import/extensions': 'off',
+    'eol-last': ['error', 'never'],
+    'import/prefer-default-export': 'off',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ['error', 4],
+  },
+};
