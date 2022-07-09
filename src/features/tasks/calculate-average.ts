d9ca8a3a5d14b79c7650f069fed850843efe7002
Lloyd Atkinson <lloydjatkinson@users.noreply.github.com>
2022-07-09 00:43:55 +0000

    chore: example task

--- src/features/tasks/calculate-average.ts
+++ src/features/tasks/calculate-average.ts
@@ -1,13 +1,6 @@
 import { createTask2 } from '../infrastructure';
 
-// export const createCalculateAverage = await createTask2({
-//     name: 'CalculateAverage',
-//     async start({ logger }) {
-//         logger.info(__filename);
-//     },
-// });
-
-export const createCalculateAverage = async () => createTask2({
+export default createTask2({
     name: 'CalculateAverage',
     async start({ logger }) {
         logger.info(__filename);
--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,6 @@
 import 'dotenv/config';
-import { createCalculateAverage } from './features/tasks/calculate-average';
-// import { createTask2 } from './features/infrastructure';
-
-// async function delay(ms) {
-//     // return await for better async stack trace support in case of errors.
-//     return new Promise((resolve) => setTimeout(resolve, ms));
-// }
+import calculateAverageTask from './features/tasks/calculate-average';
 
 (async () => {
-    // await createTask({
-    //     name: 'ExampleTask',
-    //     async start({ logger }) {
-    //         logger.info('I am inside a task');
-
-    //         setInterval(() => {
-    //             logger.info('...');
-    //         }, 1000);
-    //     },
-    // });
-
-    // await createTask({
-    //     name: 'ExampleTask',
-    //     async start({ logger }) {
-    //         logger.warn('I am inside a task as well');
-
-    //         setInterval(() => {
-    //             logger.fatal('---');
-    //         }, 4000);
-    //     },
-    // });
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    // const task = await createTask2({
-    //     name: 'ExampleTask',
-    //     async start({ logger }) {
-    //         logger.warn('I am inside a task as well');
-    //         logger.info('yay');
-    //     },
-    // });
-
-    // await task();
-
-    const calculateAverage = await createCalculateAverage();
-    await calculateAverage();
+    await calculateAverageTask;
 })();
